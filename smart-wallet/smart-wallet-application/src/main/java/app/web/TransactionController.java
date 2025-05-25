84e16e4e86c5675dc2947135920e85d298ef38e4
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-05-25 17:11:31 +0300

    Smart Wallet Advance
    new functionals for app

---
+++ smart-wallet/smart-wallet-application/src/main/java/app/web/TransactionController.java
@@ -0,0 +1,69 @@
+package app.web;
+
+import app.transaction.model.Transaction;
+import app.transaction.service.TransactionService;
+import app.user.model.User;
+import app.user.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/transactions")
+public class TransactionController {
+
+
+    private final TransactionService transactionService;
+
+
+
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+
+
+
+
+    @GetMapping
+    public ModelAndView getAllTransactions (HttpSession session){
+
+        UUID userId = (UUID) session.getAttribute ("user_id");
+        List <Transaction> transactions = transactionService.getAllByOwnerId (userId);
+
+        ModelAndView modelAndView = new ModelAndView ();
+        modelAndView.addObject ("transactions", transactions);
+        modelAndView.setViewName ("transactions");
+
+        return modelAndView;
+    }
+
+
+    @GetMapping("/{id}")
+    public ModelAndView getTransactionById (@PathVariable UUID id){
+
+        Transaction transaction = transactionService.getById (id);
+
+        ModelAndView modelAndView = new ModelAndView ();
+        modelAndView.setViewName ("transaction-result");
+        modelAndView.addObject ("transaction", transaction);
+
+
+        return modelAndView;
+        }
+
+
+
+
+}
