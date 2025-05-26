d2ce3e39bd1c1c29a39fb737760793649791e717
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-05-26 16:07:39 +0300

    updates

---
+++ smart-wallet/smart-wallet/src/main/java/app/config/BeanConfig.java
@@ -0,0 +1,19 @@
+package app.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+
+        return new BCryptPasswordEncoder ();
+    }
+}
