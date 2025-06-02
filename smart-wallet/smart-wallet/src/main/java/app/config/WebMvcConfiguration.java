0d1b80bd11d147fee10ad89621f0b626bae4cff8
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-06-02 23:42:29 +0300

    Spring security details for app

--- smart-wallet/smart-wallet/src/main/java/app/config/WebMvcConfiguration.java
+++ smart-wallet/smart-wallet/src/main/java/app/config/WebMvcConfiguration.java
@@ -2,7 +2,11 @@ package app.config;
 
 import app.security.SessionCheckInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +25,30 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         registry.addInterceptor (interceptor)
                 .addPathPatterns ("/**")
                 .excludePathPatterns ("/css/**", "/images/**");
+    }
+    // SecurityFilterChain - начин, по който Spring Security разбира как да се прилага за нашето приложение
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // authorizeHttpRequests - конфиг. за група от ендпойнти
+        // requestMatchers - достъп до даден ендпойнт
+        // .permitAll() - всеки може да достъпи този ендпойнт
+        // .anyRequest() - всички заявки, които не съм изброил
+        // .authenticated() - за да имаш достъп, трябва да си аутентикиран
+        http
+                .authorizeHttpRequests(matchers -> matchers
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/", "/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+//                        .usernameParameter("username")
+//                        .passwordParameter("password")
+                        .defaultSuccessUrl("/home", true)
+                        .failureUrl("/login?error")
+                        .permitAll());
+
+        return http.build();
     }
 }
